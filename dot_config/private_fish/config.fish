c865b4cb50865f0aa61e43925a26847805f48f5c
lltr <lltr@guardurl.com>
2023-12-22 09:46:08 +0800

    update fish config add alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,3 +3,8 @@ if status is-interactive
 end
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+export EDITOR=nvim
+
+alias editfish="chezmoi edit ~/.config/fish/config.fish"
+alias cdchezmoi="cd $(chezmoi source-path)"
